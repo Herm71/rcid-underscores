6754bc2836f33c4f460c83a8adb9f086433ab3c1
Jason Chafin <Herm71@users.noreply.github.com>
2020-10-27 16:51:39 -0700

    style: :lipstick: Responsive wrap css work

--- page-templates/full-width-template.php
+++ page-templates/full-width-template.php
@@ -10,10 +10,10 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+  <main id="main" class="site-main">
 
-		<?php
+    <?php
 		while ( have_posts() ) :
 			the_post();
 
@@ -27,9 +27,8 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+  </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
