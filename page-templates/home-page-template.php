969c68c8dc9bb59987d46f2a6d5b1e65179481e4
Jason Chafin <Herm71@users.noreply.github.com>
2020-10-28 12:54:29 -0700

    refactor: :art: Home page template work
    removed entry header and comments from template part

--- page-templates/home-page-template.php
+++ page-templates/home-page-template.php
@@ -10,26 +10,21 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+  <main id="main" class="site-main">
 
-		<?php
+    <?php
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			get_template_part( 'template-parts/content', 'home');
 
 		endwhile; // End of the loop.
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+  </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
