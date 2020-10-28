d3739eb6f42c1ee26cceb67623060a96a4df2a3d
Jason Chafin <Herm71@users.noreply.github.com>
2020-10-28 05:34:36 -0700

    style: :art: add screen-reader class to Title and Description
    Add .screen-reader class to title and description to remove from view but preserve SEO

--- header.php
+++ header.php
@@ -48,19 +48,20 @@
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-          <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+          <h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
               rel="home"><?php bloginfo( 'name' ); ?></a></h1>
           <?php
 			else :
 				?>
-          <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+          <p class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
               rel="home"><?php bloginfo( 'name' ); ?></a></p>
           <?php
 			endif;
 			$ruth_chafin_interior_design_description = get_bloginfo( 'description', 'display' );
 			if ( $ruth_chafin_interior_design_description || is_customize_preview() ) :
 				?>
-          <p class="site-description"><?php echo $ruth_chafin_interior_design_description; /* WPCS: xss ok. */ ?></p>
+          <p class="site-description screen-reader-text">
+            <?php echo $ruth_chafin_interior_design_description; /* WPCS: xss ok. */ ?></p>
           <?php endif; ?>
         </div><!-- .site-branding -->
         <section class="primary-navigation">
